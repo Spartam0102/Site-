b261fe8df07a79e81710cc59f5cb571100a2cba0
Spartam0102 foo <lubinantonin45@gmail.com>
2024-02-16 07:06:43 +0000

    fgfg

--- main.js
+++ main.js
@@ -12,3 +12,15 @@ window.onscroll = () =>{
     menu.classList.remove("fa-times")
     navbar.classList.remove("active")
 }
+
+const mtlLoader = new MTLLoader();
+mtlLoader.loadMtl('bf748742-8378-4fe3-987c-72f9c87012d3.mtl', (mtl) => {
+  mtl.preload();
+  mtl.materials.Material.side = THREE.DoubleSide;
+  objLoader.setMaterials(mtl);
+  objLoader.load('bf748742-8378-4fe3-987c-72f9c87012d3.obj', (event) => {
+    const root = event.detail.loaderRootNode;
+    scene.add(root);
+    ...
+  });
+});
